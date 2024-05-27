eb6af3338c3b8ca342b1d4efa43ecfc9f9dbef01
UzairFarooqCG <115949991+UzairFarooq1@users.noreply.github.com>
2024-05-27 14:24:30 +0300

    fix: :bug: debugging.....

--- src/components/Checkout/CheckoutComp.jsx
+++ src/components/Checkout/CheckoutComp.jsx
@@ -133,6 +133,7 @@ const CheckoutComp = ({ pendingTickets }) => {
           const phone = formData[index]?.phone_number;
           const amount = subtotal;
           const ticketId = pendingTickets[index].ticketId;
+          const eventDesc = eventData.eventDesc || "";
 
           const response = await fetch(
             "https://mpesa-backend-api.vercel.app/api/stkpush",
